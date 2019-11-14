cf07fb1fc7dfe8a3e52819a9b46c54e5880c7c50
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-13 21:10:50 -0700

    Add base starter project

---
+++ Countries-Solution/Countries-Solution/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Countries-Solution
+//
+//  Created by Mitchell Budge on 11/13/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Countries-Solution/Countries-Solution/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Countries-Solution
+//
+//  Created by Mitchell Budge on 11/13/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
